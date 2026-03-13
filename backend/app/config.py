90443f93bbada24b4006da3274a78dcbf19fb1ad
Vinith Reddy <vinithlankireddy@gmail.com>
2026-03-13 09:23:47 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,8 +6,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     """Application settings loaded from environment variables."""
 
-    DATABASE_URL: str = "postgresql://postgres.nfwqsczviswmlvchomxq:VinithCal%40369@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
-    FRONTEND_URL: str = "http://localhost:5173"
+    # FRONTEND_URL: str = "http://localhost:5173"
+    # DATABASE_URL: str = "postgresql://postgres.nfwqsczviswmlvchomxq:VinithCal%40369@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
+    DATABASE_URL: str = "postgresql+psycopg://postgres.nfwqsczviswmlvchomxq:VinithCal%40369@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
+    FRONTEND_URL: str 
 
     class Config:
         env_file = ".env"
